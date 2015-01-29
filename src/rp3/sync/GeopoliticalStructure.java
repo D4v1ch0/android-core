a52e33e853b6eabda728f9a3f6a645128dc48394
RP3-gmeregildo <gmeregildo@rp3.com>
2015-01-29 15:13:47 -0500

    Correcion a Sync de Geopolitical Structure

--- src/rp3/sync/GeopoliticalStructure.java
+++ src/rp3/sync/GeopoliticalStructure.java
@@ -19,9 +19,7 @@ public class GeopoliticalStructure {
 		WebService webService = new WebService("Core","GetGeopoliticalStructures");
 		try
 		{			
-			long time = Convert.getDotNetTicksFromDate(SyncAudit.getLastSyncDate(SyncAdapter.SYNC_EVENT_SUCCESS));
 			webService.addCurrentAuthToken();
-			webService.addParameter("@FecModParameter", time);
 			
 			try {
 				webService.invokeWebService();
@@ -37,6 +35,68 @@ public class GeopoliticalStructure {
 			
 			rp3.data.models.GeopoliticalStructureType.deleteAll(db, Contract.GeopoliticalStructureType.TABLE_NAME);
 			rp3.data.models.GeopoliticalStructure.deleteAll(db, Contract.GeopoliticalStructure.TABLE_NAME);
+			
+			for(int i=0; i < types.length(); i++){
+				
+				try {
+					JSONObject type = types.getJSONObject(i);
+					rp3.data.models.GeopoliticalStructureType modelType = new GeopoliticalStructureType();
+					modelType.setName(type.getString("N"));
+					modelType.setID(type.getLong("Id"));
+					modelType.setLevelStructure(type.getInt("Lev"));
+					
+					rp3.data.models.GeopoliticalStructureType.insert(db, modelType);
+					
+					JSONArray strs = type.getJSONArray("Content");
+					
+					for(int j=0; j < strs.length(); j++){
+						JSONObject str = strs.getJSONObject(j);
+						rp3.data.models.GeopoliticalStructure modelStr = new rp3.data.models.GeopoliticalStructure();						
+						modelStr.setID(str.getLong("Id"));
+						modelStr.setIsoCode(str.getString("Iso"));
+						modelStr.setGeopoliticalStructureTypeId(str.getInt("TId"));
+						modelStr.setLatitude( str.isNull("La") ? null : Double.parseDouble(str.getString("La")));
+						modelStr.setLongitude( str.isNull("Lo") ? null : Double.parseDouble(str.getString("Lo")));
+						modelStr.setName(str.getString("N"));
+						modelStr.setParents(str.getString("P"));
+						modelStr.setParentGeopoliticalStructureId(str.isNull("PId") ? 0 : str.getLong("PId") );
+						
+						rp3.data.models.GeopoliticalStructure.insert(db, modelStr);
+					}
+					
+					
+					
+				} catch (JSONException e) {
+					
+					return SyncAdapter.SYNC_EVENT_ERROR;
+				}
+				
+			}
+		}finally{
+			webService.close();
+		}
+		
+		return SyncAdapter.SYNC_EVENT_SUCCESS;		
+	}
+	
+	public static int executeSyncLastUpdate(DataBase db, long time){
+		WebService webService = new WebService("Core","GetGeopoliticalStructures");
+		try
+		{			
+			webService.addCurrentAuthToken();
+			webService.addParameter("@lastUpdate", time);
+			
+			try {
+				webService.invokeWebService();
+			} catch (HttpResponseException e) {
+				if(e.getStatusCode() == HttpConnection.HTTP_STATUS_UNAUTHORIZED)
+					return SyncAdapter.SYNC_EVENT_AUTH_ERROR;
+				return SyncAdapter.SYNC_EVENT_HTTP_ERROR;
+			} catch (Exception e) {
+				return SyncAdapter.SYNC_EVENT_ERROR;
+			}
+			
+			JSONArray types = webService.getJSONArrayResponse();			
 			
 			for(int i=0; i < types.length(); i++){
 				

88317e524e6f4d765f85085325abda7998ab88bb
RP3-gmeregildo <gmeregildo@rp3.com.ec>
2015-09-30 16:11:45 -0500

    Correcciones

--- src/rp3/configuration/AppConfiguration.java
+++ src/rp3/configuration/AppConfiguration.java
@@ -7,7 +7,8 @@ public class AppConfiguration {
 	public static final String KEY_HELP = "help";
     public static final String KEY_MAIL = "mail";
 	public static final String KEY_ALLOW_SETTINGS_ON_AUT = "allowSettingsOnAut";
-	public static final String KEY_ALLOW_HELP_ON_AUT = "allowHelpOnAut";	
+	public static final String KEY_ALLOW_HELP_ON_AUT = "allowHelpOnAut";
+	public static final String KEY_WEB_KEY = "webkey";
 	
 	private ContentValues configurations;	
 	
@@ -43,6 +44,10 @@ public class AppConfiguration {
 	
 	public boolean allowHelpOnAut(){
 		return "TRUE".equals(get(KEY_ALLOW_HELP_ON_AUT));
+	}
+
+	public String getWebKey(){
+		return get(KEY_WEB_KEY);
 	}
 	
 }

de08bf7e29df7af7f2030c9d1437578a975e74cb
RP3-gmeregildo <gmeregildo@rp3.com.ec>
2015-11-13 11:48:47 -0500

    Correcciones

--- src/rp3/app/NotificationActivity.java
+++ src/rp3/app/NotificationActivity.java
@@ -1,7 +1,10 @@
 package rp3.app;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Window;
+
+import java.util.Locale;
 
 import rp3.core.R;
 import rp3.util.NotificationPusher;
@@ -11,6 +14,7 @@ import rp3.util.NotificationPusher;
  */
 public class NotificationActivity extends BaseActivity {
 
+    TextToSpeech t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +22,25 @@ public class NotificationActivity extends BaseActivity {
         setContentView(R.layout.activity_notification);
 
         String title = getIntent().getExtras().getString(NotificationPusher.TAG_TITLE);
-        String message = getIntent().getExtras().getString(NotificationPusher.TAG_MESSAGE);
+        final String message = getIntent().getExtras().getString(NotificationPusher.TAG_MESSAGE);
         String footer = getIntent().getExtras().getString(NotificationPusher.TAG_FOOTER);
 
         setTextViewText(R.id.title, title);
         setTextViewText(R.id.message, message);
         setTextViewText(R.id.footer, footer);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    try {
+                        t1.setLanguage(new Locale("es", "ES"));
+                        t1.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+                    } catch (Exception ex)
+                    {}
+                }
+            }
+        });
 
     }
 }

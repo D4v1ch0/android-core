fb3dac76a9128539db0104bc0220c4265adbdb13
RP3-gmeregildo <gmeregildo@rp3.com.ec>
2016-02-17 15:08:10 -0500

    Correcciones

--- src/rp3/util/StringUtils.java
+++ src/rp3/util/StringUtils.java
@@ -8,4 +8,45 @@ public class StringUtils {
     {
         return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
+
+    public static String centerStringInLine(String text, int spaces){
+        if(text.length() >= spaces)
+            return text + '\n';
+        else {
+            int whiteSpaces = spaces - text.length();
+            if (whiteSpaces % 2 == 1) {
+                whiteSpaces = whiteSpaces - 1;
+            }
+            int spacesToCenter = whiteSpaces / 2;
+            for (int i = 1; i <= spacesToCenter; i++)
+                text = " " + text + " ";
+            return text + '\n';
+        }
+    }
+
+    public static String rightStringInSpace(String text, int spaces)
+    {
+        if(text.length() >= spaces)
+            return text;
+        else
+        {
+            int whitespaces = spaces - text.length();
+            for(int i = 1; i <= whitespaces; i++)
+                text = " " + text;
+            return text;
+        }
+    }
+
+    public static String leftStringInSpace(String text, int spaces)
+    {
+        if(text.length() >= spaces)
+            return text;
+        else
+        {
+            int whitespaces = spaces - text.length();
+            for(int i = 1; i <= whitespaces; i++)
+                text = text + " ";
+            return text;
+        }
+    }
 }
